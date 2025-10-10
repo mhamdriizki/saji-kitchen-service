90540e0cd7df552e37cfcdc446ced282170ec59d
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-10-11 00:51:23 +0700

    fix : Change timezone to Asia/Jakarta, remove hardcode in orderService

--- src/main/java/com/sajikitchen/saji_cashier/SajiCashierApplication.java
+++ src/main/java/com/sajikitchen/saji_cashier/SajiCashierApplication.java
@@ -1,10 +1,18 @@
 package com.sajikitchen.saji_cashier;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SajiCashierApplication {
+	@PostConstruct
+	public void init() {
+		// Mengatur default timezone untuk seluruh aplikasi ke WIB (UTC+7)
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SajiCashierApplication.class, args);
